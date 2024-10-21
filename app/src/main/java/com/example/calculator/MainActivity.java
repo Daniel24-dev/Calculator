c5baec06867b00b82a6f6a948aec0921793e6722
Daniel24-dev <odintor18@gmail.com>
2024-10-22 03:51:24 +0900

    added second activity

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,95 +1,91 @@
 package com.example.calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
-    private Integer firstNumber, secondNumber, result;
-    private Boolean isOperationClick;
-    private String currentOperation ;
+    private Integer firstNumber, secondNumber;
+    private Integer result;;
 
+    private Boolean isOperationClick;
+    private String currentOperation;
+    private Button resultBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
-       setContentView(R.layout.activity_main);
-        // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-       //     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-         //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        //    return insets;
-       // });
-        textView= findViewById(R.id.main_textview);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.main_textview);
+        resultBtn = findViewById(R.id.result_btn);
         currentOperation = null;
+        isOperationClick = false;
+
+        resultBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, secondActivity.class);
+            intent.putExtra("result", result.toString());
+            startActivity(intent);
+        });
     }
 
-  public void onNumberClick(View view) {
-      String text = ((MaterialButton) view).getText().toString();
-      if (text.equals("AC")){
-          textView.setText("0");
-      }else if(textView.getText().toString().equals("0") || isOperationClick){
-        textView.setText(text);
-      }else {
-          textView.append(text);
-      }
-      isOperationClick = false;
+    public void onNumberClick(View view) {
+        String text = ((MaterialButton) view).getText().toString();
+        resultBtn.setVisibility(View.GONE);
+        if (text.equals("AC")) {
+            textView.setText("0");
+        } else if (textView.getText().toString().equals("0") || isOperationClick) {
+            textView.setText(text);
+        } else {
+            textView.append(text);
+        }
+        isOperationClick = false;
     }
 
     public void onOperationClick(View view) {
-
-        if (view.getId() == R.id.btn_plus){
-            firstNumber = Integer.valueOf(textView.getText().toString());
-            currentOperation = "+";
-        } else if (view.getId() == R.id.btn_minus) {
-            firstNumber = Integer.valueOf(textView.getText().toString());
-            currentOperation = "-";
-
-        } else if (view.getId() == R.id.btn_multiple) {
-            firstNumber = Integer.valueOf(textView.getText().toString());
-            currentOperation = "*";
-
-        }else if (view.getId() == R.id.btn_divide) {
-            firstNumber = Integer.valueOf(textView.getText().toString());
-            currentOperation = "/";
-
-        }else if (view.getId() == R.id.btn_percent){
-            firstNumber = Integer.valueOf(textView.getText().toString());
-            currentOperation = "%";
-        }
-        else if (view.getId()==R.id.dot){
-            firstNumber = Integer.valueOf(textView.getText().toString()+ ".");
-        }
-        else if (view.getId() == R.id.btn_equal) {
+        if (view.getId() == R.id.btn_equal) {
             secondNumber = Integer.valueOf(textView.getText().toString());
-            if (currentOperation.equals("+")){
-                Integer result = firstNumber + secondNumber;
-                textView.setText(result.toString());
+            resultBtn.setVisibility(View.VISIBLE);
+
+            if (currentOperation.equals("+")) {
+                result = firstNumber + secondNumber;
             } else if (currentOperation.equals("-")) {
-                Integer result = firstNumber - secondNumber;
-                textView.setText(result.toString());
-            }else if (currentOperation.equals("*")) {
-                Integer result = firstNumber * secondNumber;
-                textView.setText(result.toString());
-            }else if (currentOperation.equals("/")) {
-                Integer result = firstNumber / secondNumber;
-                textView.setText(result.toString());
+                result = firstNumber - secondNumber;
+            } else if (currentOperation.equals("*")) {
+                result = firstNumber * secondNumber;
+            } else if (currentOperation.equals("/")) {
+                if (secondNumber != 0) {
+                    result = firstNumber / secondNumber;
+                } else {
+                    textView.setText("Ошибка");
+                    return;
+                }
             }
 
+            textView.setText(result.toString());
+        } else {
+            firstNumber = Integer.valueOf(textView.getText().toString());
 
+            if (view.getId() == R.id.btn_plus) {
+                currentOperation = "+";
+            } else if (view.getId() == R.id.btn_minus) {
+                currentOperation = "-";
+            } else if (view.getId() == R.id.btn_multiple) {
+                currentOperation = "*";
+            } else if (view.getId() == R.id.btn_divide) {
+                currentOperation = "/";
+            } else if (view.getId() == R.id.btn_percent) {
+                currentOperation = "%";
+            }
+
+            isOperationClick = true;
         }
-        isOperationClick = true;
-
     }
-
 }
